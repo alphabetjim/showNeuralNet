766cc2f4a200e3760cb2c5d53e7551bb85fbe74c
alphabetjim <james.bcourt@outlook.com>
2024-05-14 21:37:47 +0100

    Front-end functional. Bare bones styling only.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///nnimages.db'
+
 db = SQLAlchemy(app)
 
 class Image(db.Model):
@@ -14,12 +15,35 @@ class Image(db.Model):
 	def __repr__(self):
 		return '<Image %r>' % self.id
 
-# with app.app_context():
-# 	db.create_all()
+with app.app_context():
+	db.create_all()
 
 @app.route('/', methods = ['POST', 'GET'])
 def index():
 	return render_template('index.html')
 
+@app.route('/upload', methods = ['POST', 'GET'])
+def upload():
+	if request.method == "POST":
+		if 'image' not in request.files:
+			return "No image uploaded", 400
+		image_file = request.files['image']
+		if image_file:
+			# Save the image file to a folder on your server
+			image_path = f"static/images/{image_file.filename}"
+			image_file.save(image_path)
+	        
+	        # Store the image path in the database
+			new_image = Image(filename=image_path)
+			db.session.add(new_image)
+			db.session.commit()
+	        
+			return render_template('uploaded.html', image = new_image)
+		else:
+			return "Invalid file format", 400
+	else:
+		return "You probably won't see this."
+
 if __name__ == "__main__":
     app.run(debug=True)
+    db.create_all()
